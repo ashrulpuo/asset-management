e5c1d2f238cb7e0f35e617ccecdff74861ac08aa
ashrulpuo <mohdashrul70@gmail.com>
2019-01-27 00:55:58 +0800

    set template

---
+++ resources/views/nav/admin.blade.php
@@ -0,0 +1,51 @@
+
+<ul class="nav">
+  <li class="nav-item active  ">
+    <a class="nav-link" href="./dashboard.html">
+      <i class="material-icons">dashboard</i>
+      <p>Dashboard</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./user.html">
+      <i class="material-icons">business</i>
+      <p>Vendor</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./tables.html">
+      <i class="material-icons">contacts</i>
+      <p>Contact Person</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./typography.html">
+      <i class="material-icons">control_point</i>
+      <p>Lookup Assets</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./icons.html">
+      <i class="material-icons">picture_as_pdf</i>
+      <p>Reporting</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./map.html">
+      <i class="material-icons">computer</i>
+      <p>Assets</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./notifications.html">
+      <i class="material-icons">business</i>
+      <p>Company</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./rtl.html">
+      <i class="material-icons">picture_as_pdf</i>
+      <p>Reporting</p>
+    </a>
+  </li>
+</ul>
---
+++ resources/views/nav/vendor.blade.php
@@ -0,0 +1,26 @@
+ <ul class="nav">
+  <li class="nav-item active  ">
+    <a class="nav-link" href="./dashboard.html">
+      <i class="material-icons">dashboard</i>
+      <p>Dashboard</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./map.html">
+      <i class="material-icons">computer</i>
+      <p>Assets</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./notifications.html">
+      <i class="material-icons">business</i>
+      <p>Company</p>
+    </a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="./rtl.html">
+      <i class="material-icons">picture_as_pdf</i>
+      <p>Reporting</p>
+    </a>
+  </li>
+</ul>
