13e30ee0312d645d1ebb92bdf98d186c405a127d
ashrulpuo <mohdashrul70@gmail.com>
2019-01-27 07:24:45 +0800

    add datatables plugin

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+// Route::get('/', function () {
+//     return view('dashboard');
+// });
+
+Route::get('/', 'HomeController@index');
